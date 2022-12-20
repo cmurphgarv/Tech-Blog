68d5699304ad0ed21b8019e328c935cb84adc5bf
Charlotte Murphey <murphey.charlotte@gmail.com>
2022-12-20 10:37:00 -0500

    more route editing (based on project 2)

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -6,31 +6,50 @@ const Blog = require('../../models/Blog');
 // The api/blogs endpoint
 
 // GET all blogs
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const userData = await Blog.findAll({
+      include: [{ model: User }, { model: Comment }],
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+  // GET one blog by ID
+  router.get("/:id", async (req, res) => {
     try {
-      const blogData = await Blog.findAll({
-        include: [{ model: User }],
+      const userData = await Blog.findByPk(req.params.id, {
+        include: [{ model: User }, { model: Comment }],
       });
-      res.status(200).json(blogData);
+  
+      if (!userData) {
+        res.status(404).json({ message: "No user found with that id!" });
+        return;
+      }
+  
+      res.status(200).json(userData);
     } catch (err) {
       res.status(500).json(err);
     }
   });
 
-  // GET one blog by ID
-router.get('/:id', async (req, res) => {
+  // CREATE new blog
+router.post("/", async (req, res) => {
   try {
-    const blogData = await Blog.findByPk(req.params.id, {
-      include: [{ model: User }],
+    const dbBlogData = await Blog.create({
+      title: req.body.title,
+      createdAt: req.body.description,
+      post_content: req.body.deadline,
+      user_id: req.body.user_id,
     });
-
-    if (!blogData) {
-      res.status(404).json({ message: 'No blog found with that id!' });
-      return;
-    }
-
-    res.status(200).json(blogData);
+    res.status(200).json(dbBlogData);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
+
+module.exports = router;
